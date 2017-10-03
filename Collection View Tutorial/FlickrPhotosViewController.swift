c7b527620730c35634c1a66a7769ea968690de87
Łukasz Sokołowski <lukasz.m.sokolowski1995@gmail.com>
2017-10-03 15:38:05 +0200

    Collection View Controller. Navigation Controller. Raywenderlich theme color. Subclassing UICollectionViewController. Set the identifier for a cell.

---
+++ Collection View Tutorial/FlickrPhotosViewController.swift
@@ -0,0 +1,15 @@
+//
+//  FlickrPhotosViewController.swift
+//  Collection View Tutorial
+//
+//  Created by Łukasz Sokołowski on 03/10/2017.
+//  Copyright © 2017 Łukasz Sokołowski. All rights reserved.
+//
+
+import UIKit
+
+final class FlickrPhotosViewController: UICollectionViewController {
+
+  //MARK: - Properties
+    fileprivate let reuseIdentifier = "FlickrCell"
+}
